cd3280988ff069d6cc00e96108ae068740feb3e6
Efaideleon <109036920+Efai-De-Leon@users.noreply.github.com>
2024-02-22 11:21:15 -0800

    string sort

--- HackerRank/string_permutations.c
+++ HackerRank/string_permutations.c
@@ -5,7 +5,8 @@
 int swap_last_two_strings(int n, char **s);
 int swap_previous_with_next_in_order(int n, char **s);
 int get_index_of_next_in_order_value(int n, int from_value_index, char **s);
-int set_remaining_values_in_order(int n, char **s, int from_value_index);
+void string_sort(char** arr, const int len, int (*cmp_func)(const char* a, const char* b), int from_index);
+int lexicographic_sort(const char* a, const char* b);
 
 int next_permutation(int n, char **s)
 {
@@ -14,31 +15,30 @@ int next_permutation(int n, char **s)
      * Return 0 when there is no next permutation and 1 otherwise
      * Modify array s to its next permutation
      */
+    int index_prev_value;
     if (swap_last_two_strings(n, s) == 0)
     {
-        swap_previous_with_next_in_order(n, s);
+        index_prev_value = swap_previous_with_next_in_order(n, s);
+        if (index_prev_value)
+        {
+            printf("%d\n", index_prev_value);
+            string_sort(s, n, lexicographic_sort, index_prev_value + 1);
+        }
     }
     return 1;
-}
-
-int set_remaining_values_in_order(int n, char **s, int from_value_index)
-{
-    for (int i = 0; i < n; i++)
-    {
-
-    }
 }
 
 int swap_previous_with_next_in_order(int n, char **s)
 {
     char *prev_value;
     int index_of_next_value;
+    int index_of_prev_value = 0;
     for (int i = 0; i < n - 1; i++)
     {
         if (s[i][0] > s[i + 1][0])
         {
             prev_value = s[i - 1];
-
+            index_of_prev_value = i - 1;
             // find the next in order
             // if can't find it's program is over
             index_of_next_value = get_index_of_next_in_order_value(n, i - 1, s);
@@ -50,7 +50,7 @@ int swap_previous_with_next_in_order(int n, char **s)
             s[index_of_next_value] = prev_value;
         }
     }
-    return 1;
+    return index_of_prev_value;
 }
 
 int get_index_of_next_in_order_value(int n, int from_value_index, char **s)
@@ -96,6 +96,67 @@ int swap_last_two_strings(int n, char **s)
                 s[i + 1] = temp;
                 return 1;
             }
+        }
+    }
+    return 0;
+}
+
+void string_sort(char** arr, const int len, int (*cmp_func)(const char* a, const char* b), int from_index)
+{
+    // arr: is the array of strings to sort
+    // len: how many strings the arr has
+    char *temp_arr = NULL;
+    for (int i = from_index; i < len; i++)
+        {
+        if (i + 1 < len - 1)
+        {
+            if (cmp_func(arr[i], arr[i + 1]) == 0)
+            {
+                temp_arr = arr[i];
+                arr[i] = (char *) realloc(arr[i + 1], sizeof(char) * strlen(arr[i+1]));
+                arr[i] = arr[i + 1];
+                arr[i + 1] = (char *) realloc(temp_arr, sizeof(char) * strlen(temp_arr));
+                arr[i + 1] = temp_arr;
+            }
+        }
+
+        for (int j = i; j >= 1; j--)
+        {
+            if (cmp_func(arr[j], arr[j - 1]) == 1)
+            {
+                temp_arr = arr[j];
+                arr[j] = (char *) realloc(arr[j - 1], sizeof(char) * strlen(arr[j - 1]));
+                arr[j] = arr[j - 1];
+                arr[j - 1] = (char *) realloc(temp_arr, sizeof(char) * strlen(temp_arr));
+                arr[j - 1] = temp_arr;
+            }
+        }
+    }
+}
+
+int lexicographic_sort(const char* a, const char* b)
+{
+    // a: is a string passed by reference that will be printed later
+    // return 0: false, 1: true
+    int max_len = 0;
+    if (strlen(a) > strlen(b))
+    {
+        max_len = strlen(a);
+    }
+    else
+    {
+        max_len = strlen(b);
+    }
+
+    for (int i = 0; i < max_len; i++)
+    {
+        if (a[i] < b[i])
+        {
+            return 1;
+        }
+        else if (a[i] > b[i])
+        {
+            return 0;
         }
     }
     return 0;
